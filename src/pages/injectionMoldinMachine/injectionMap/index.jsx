27b0b6c6e3ec9dc31ca76ff33ff43339c8f62361
nhien <iambravo680@gmail.com>
2022-05-02 13:51:14 +0700

    show modal when hover in injection map, change animation time

--- src/pages/injectionMoldinMachine/injectionMap/index.jsx
+++ src/pages/injectionMoldinMachine/injectionMap/index.jsx
@@ -5,6 +5,83 @@ import { ReactComponent as VerticalMachine } from '../../../assets/images/inject
 import { ReactComponent as HorizontalMachine } from '../../../assets/images/injectionMoldingMachine/injectionFullDetail.svg';
 import ReportNavigationButton from '../../../components/reportNavigationButton/ReportNavigationButton';
 import { INJECTION_MACHINE_LAYOUT } from '../../../utils/utils';
+import InjectionMoldingMachine from '../../../components/injectionMoldingMachine/InjectionMoldingMachine';
+
+function InjectionMachine({ item, direction, modalPosition = [] }) {
+	//fake data
+	const data = {
+		name: 'AXb15',
+		number: 'M1',
+		percent: 30,
+		state: 'R',
+		cycle: '30 giây',
+		openDoorTime: '7 giây',
+		productId: 'EE2003',
+		wattage: 'small',
+	};
+
+	return (
+		<React.Fragment>
+			{item.isHaitian ? (
+				<Link className="injection-map__map-item-inner" to={item.url}>
+					<div className="injection-map__map-item-title">{item.title}</div>
+					{item.subTitle.includes('\n') ? (
+						<div className="injection-map__map-item-sub-title">
+							{item.subTitle.split('\n').map((subTitle, index) => {
+								return (
+									<React.Fragment key={index}>
+										{subTitle}
+										{index !== item.subTitle.split('\n').length - 1 ? <br /> : null}
+									</React.Fragment>
+								);
+							})}
+						</div>
+					) : (
+						<div className="injection-map__map-item-sub-title">{item.subTitle}</div>
+					)}
+					<div className="injection-map__machine-container">
+						{direction === 'vectical' ? (
+							<VerticalMachine height="100px" width="100%" />
+						) : (
+							<HorizontalMachine height="100px" width="100%" />
+						)}
+						<div className={`card injection-map__map-item-modal ${modalPosition.join(' ')}`}>
+							<InjectionMoldingMachine injectionMoldingMachineData={data} />
+						</div>
+					</div>
+				</Link>
+			) : (
+				<div className="injection-map__map-item-inner injection-map__map-item-inner--none-haitian">
+					<div className="injection-map__map-item-title">{item.title}</div>
+					{item.subTitle.includes('\n') ? (
+						<div className="injection-map__map-item-sub-title">
+							{item.subTitle.split('\n').map((subTitle, index) => {
+								return (
+									<React.Fragment key={index}>
+										{subTitle}
+										{index !== item.subTitle.split('\n').length - 1 ? <br /> : null}
+									</React.Fragment>
+								);
+							})}
+						</div>
+					) : (
+						<div className="injection-map__map-item-sub-title">{item.subTitle}</div>
+					)}
+					<div className="injection-map__machine-container">
+						{direction === 'vectical' ? (
+							<VerticalMachine height="100px" width="100%" />
+						) : (
+							<HorizontalMachine height="100px" width="100%" />
+						)}
+						<div className={`card injection-map__map-item-modal ${modalPosition.join(' ')}`}>
+							<InjectionMoldingMachine injectionMoldingMachineData={data} />
+						</div>
+					</div>
+				</div>
+			)}
+		</React.Fragment>
+	);
+}
 
 function InjectionMap() {
 	const history = useHistory();
@@ -23,51 +100,12 @@ function InjectionMap() {
 										<div className="injection-map__map-item">
 											{INJECTION_MACHINE_LAYOUT[0].map((item, index) => {
 												return (
-													<React.Fragment key={index}>
-														{item.isHaitian ? (
-															<Link className="injection-map__map-item-inner" to={item.url}>
-																<div className="injection-map__map-item-title">{item.title}</div>
-																{item.subTitle.includes('\n') ? (
-																	<div className="injection-map__map-item-sub-title">
-																		{item.subTitle.split('\n').map((subTitle, index) => {
-																			return (
-																				<React.Fragment key={index}>
-																					{subTitle}
-																					{index !== item.subTitle.split('\n').length - 1 ? <br /> : null}
-																				</React.Fragment>
-																			);
-																		})}
-																	</div>
-																) : (
-																	<div className="injection-map__map-item-sub-title">{item.subTitle}</div>
-																)}
-																<div className="injection-map__machine-container">
-																	<VerticalMachine height="100px" width="100%" />
-																</div>
-															</Link>
-														) : (
-															<div className="injection-map__map-item-inner injection-map__map-item-inner--none-haitian">
-																<div className="injection-map__map-item-title">{item.title}</div>
-																{item.subTitle.includes('\n') ? (
-																	<div className="injection-map__map-item-sub-title">
-																		{item.subTitle.split('\n').map((subTitle, index) => {
-																			return (
-																				<React.Fragment key={index}>
-																					{subTitle}
-																					{index !== item.subTitle.split('\n').length - 1 ? <br /> : null}
-																				</React.Fragment>
-																			);
-																		})}
-																	</div>
-																) : (
-																	<div className="injection-map__map-item-sub-title">{item.subTitle}</div>
-																)}
-																<div className="injection-map__machine-container">
-																	<VerticalMachine height="100px" width="100%" />
-																</div>
-															</div>
-														)}
-													</React.Fragment>
+													<InjectionMachine
+														item={item}
+														key={index}
+														direction="vectical"
+														modalPosition={index <= 7 ? ['bottom', 'right'] : ['bottom', 'left']}
+													/>
 												);
 											})}
 										</div>
@@ -76,51 +114,12 @@ function InjectionMap() {
 										<div className="injection-map__map-item">
 											{INJECTION_MACHINE_LAYOUT[1].map((item, index) => {
 												return (
-													<React.Fragment key={index}>
-														{item.isHaitian ? (
-															<Link className="injection-map__map-item-inner" to={item.url}>
-																<div className="injection-map__map-item-title">{item.title}</div>
-																{item.subTitle.includes('\n') ? (
-																	<div className="injection-map__map-item-sub-title">
-																		{item.subTitle.split('\n').map((subTitle, index) => {
-																			return (
-																				<React.Fragment key={index}>
-																					{subTitle}
-																					{index !== item.subTitle.split('\n').length - 1 ? <br /> : null}
-																				</React.Fragment>
-																			);
-																		})}
-																	</div>
-																) : (
-																	<div className="injection-map__map-item-sub-title">{item.subTitle}</div>
-																)}
-																<div className="injection-map__machine-container">
-																	<VerticalMachine height="100px" width="100%" />
-																</div>
-															</Link>
-														) : (
-															<div className="injection-map__map-item-inner injection-map__map-item-inner--none-haitian">
-																<div className="injection-map__map-item-title">{item.title}</div>
-																{item.subTitle.includes('\n') ? (
-																	<div className="injection-map__map-item-sub-title">
-																		{item.subTitle.split('\n').map((subTitle, index) => {
-																			return (
-																				<React.Fragment key={index}>
-																					{subTitle}
-																					{index !== item.subTitle.split('\n').length - 1 ? <br /> : null}
-																				</React.Fragment>
-																			);
-																		})}
-																	</div>
-																) : (
-																	<div className="injection-map__map-item-sub-title">{item.subTitle}</div>
-																)}
-																<div className="injection-map__machine-container">
-																	<VerticalMachine height="100px" width="100%" />
-																</div>
-															</div>
-														)}
-													</React.Fragment>
+													<InjectionMachine
+														item={item}
+														key={index}
+														direction="vectical"
+														modalPosition={index <= 7 ? ['bottom', 'right'] : ['bottom', 'left']}
+													/>
 												);
 											})}
 										</div>
@@ -129,102 +128,24 @@ function InjectionMap() {
 										<div className="injection-map__map-first-column">
 											{INJECTION_MACHINE_LAYOUT[2][0].map((item, index) => {
 												return (
-													<React.Fragment key={index}>
-														{item.isHaitian ? (
-															<Link className="injection-map__map-item-inner--horizontal" to={item.url}>
-																<div className="injection-map__map-item-title">{item.title}</div>
-																{item.subTitle.includes('\n') ? (
-																	<div className="injection-map__map-item-sub-title">
-																		{item.subTitle.split('\n').map((subTitle, index) => {
-																			return (
-																				<React.Fragment key={index}>
-																					{subTitle}
-																					{index !== item.subTitle.split('\n').length - 1 ? <br /> : null}
-																				</React.Fragment>
-																			);
-																		})}
-																	</div>
-																) : (
-																	<div className="injection-map__map-item-sub-title">{item.subTitle}</div>
-																)}
-																<div className="injection-map__machine-container">
-																	<HorizontalMachine height="100%" width="200px" />
-																</div>
-															</Link>
-														) : (
-															<div className="injection-map__map-item-inner--horizontal injection-map__map-item-inner--none-haitian">
-																<div className="injection-map__map-item-title">{item.title}</div>
-																{item.subTitle.includes('\n') ? (
-																	<div className="injection-map__map-item-sub-title">
-																		{item.subTitle.split('\n').map((subTitle, index) => {
-																			return (
-																				<React.Fragment key={index}>
-																					{subTitle}
-																					{index !== item.subTitle.split('\n').length - 1 ? <br /> : null}
-																				</React.Fragment>
-																			);
-																		})}
-																	</div>
-																) : (
-																	<div className="injection-map__map-item-sub-title">{item.subTitle}</div>
-																)}
-																<div className="injection-map__machine-container">
-																	<HorizontalMachine height="100%" width="200px" />
-																</div>
-															</div>
-														)}
-													</React.Fragment>
+													<InjectionMachine
+														item={item}
+														key={index}
+														direction="horizontal"
+														modalPosition={index <= 2 ? ['bottom', 'right'] : ['top', 'right']}
+													/>
 												);
 											})}
 										</div>
 										<div className="injection-map__map-second-column">
 											{INJECTION_MACHINE_LAYOUT[2][1].map((item, index) => {
 												return (
-													<React.Fragment key={index}>
-														{item.isHaitian ? (
-															<Link className="injection-map__map-item-inner--horizontal" to={item.url}>
-																<div className="injection-map__map-item-title">{item.title}</div>
-																{item.subTitle.includes('\n') ? (
-																	<div className="injection-map__map-item-sub-title">
-																		{item.subTitle.split('\n').map((subTitle, index) => {
-																			return (
-																				<React.Fragment key={index}>
-																					{subTitle}
-																					{index !== item.subTitle.split('\n').length - 1 ? <br /> : null}
-																				</React.Fragment>
-																			);
-																		})}
-																	</div>
-																) : (
-																	<div className="injection-map__map-item-sub-title">{item.subTitle}</div>
-																)}
-																<div className="injection-map__machine-container">
-																	<HorizontalMachine height="100%" width="200px" />
-																</div>
-															</Link>
-														) : (
-															<div className="injection-map__map-item-inner--horizontal injection-map__map-item-inner--none-haitian">
-																<div className="injection-map__map-item-title">{item.title}</div>
-																{item.subTitle.includes('\n') ? (
-																	<div className="injection-map__map-item-sub-title">
-																		{item.subTitle.split('\n').map((subTitle, index) => {
-																			return (
-																				<React.Fragment key={index}>
-																					{subTitle}
-																					{index !== item.subTitle.split('\n').length - 1 ? <br /> : null}
-																				</React.Fragment>
-																			);
-																		})}
-																	</div>
-																) : (
-																	<div className="injection-map__map-item-sub-title">{item.subTitle}</div>
-																)}
-																<div className="injection-map__machine-container">
-																	<HorizontalMachine height="100%" width="200px" />
-																</div>
-															</div>
-														)}
-													</React.Fragment>
+													<InjectionMachine
+														item={item}
+														key={index}
+														direction="horizontal"
+														modalPosition={index <= 2 ? ['bottom', 'left'] : ['top', 'left']}
+													/>
 												);
 											})}
 										</div>
