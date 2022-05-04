76603c495ffeff3af57716ef465f85ca1abcb068
Tri Hoang Minh <tri.hoang.hcmut@gmail.com>
2022-05-04 11:43:34 +0700

    Add plan tracking route

---
+++ src/pages/planTracking/trackingDaily/TrackingDaily.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TrackingDaily() {
+	return <div>TrackingDaily</div>;
+}
+
+export default TrackingDaily;
---
+++ src/pages/planTracking/trackingMonthly/TrackingMonthly.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TrackingMonthly() {
+	return <div>TrackingMonthly</div>;
+}
+
+export default TrackingMonthly;
