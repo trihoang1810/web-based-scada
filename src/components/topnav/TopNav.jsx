18c5fd02e78b82ef677c53b09e820eef815d81ae
Tri Hoang Minh <tri.hoang.hcmut@gmail.com>
2022-05-05 14:38:30 +0700

    Remove permanently user image and notifications in topnav component

--- src/components/topnav/TopNav.jsx
+++ src/components/topnav/TopNav.jsx
@@ -1,54 +1,54 @@
 import React from 'react';
 
-import Dropdown from '../dropdown/DropDown';
+// import Dropdown from '../dropdown/DropDown';
 
 import { NavLink } from 'react-router-dom';
 
 import './topnav.css';
 
-import notifications from '../../assets/JsonData/notification.json';
+// import notifications from '../../assets/JsonData/notification.json';
 
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
-import user_image from '../../assets/images/user.jpg';
+// import user_image from '../../assets/images/user.jpg';
 
 import data_results from '../../assets/JsonData/sidebar_routes.json';
 
-import user_menu from '../../assets/JsonData/user_menus.json';
+// import user_menu from '../../assets/JsonData/user_menus.json';
 
 import { useSelector } from 'react-redux';
 
-const curr_user = {
-	display_name: 'Tri Hoang',
-	image: user_image,
-};
+// const curr_user = {
+// 	display_name: 'Tri Hoang',
+// 	image: user_image,
+// };
 
-const renderNotificationItem = (item, index) => (
-	<div className="notification-item" key={index}>
-		<i className={item.icon}></i>
-		<span>{item.content}</span>
-	</div>
-);
+// const renderNotificationItem = (item, index) => (
+// 	<div className="notification-item" key={index}>
+// 		<i className={item.icon}></i>
+// 		<span>{item.content}</span>
+// 	</div>
+// );
 
-const renderUserToggle = (user) => (
-	<div className="topnav__right-user">
-		<div className="topnav__right-user__image">
-			<img src={curr_user.image} alt="ảnh người dùng" />
-		</div>
-		<div className="topnav__right-user__name">{user.display_name}</div>
-	</div>
-);
+// const renderUserToggle = (user) => (
+// 	<div className="topnav__right-user">
+// 		<div className="topnav__right-user__image">
+// 			<img src={curr_user.image} alt="ảnh người dùng" />
+// 		</div>
+// 		<div className="topnav__right-user__name">{user.display_name}</div>
+// 	</div>
+// );
 
-const renderUserMenu = (item, i) => {
-	return (
-		<Link to="/" key={i}>
-			<div className="notification-item">
-				<i className={item.icon}></i>
-				<span>{item.content}</span>
-			</div>
-		</Link>
-	);
-};
+// const renderUserMenu = (item, i) => {
+// 	return (
+// 		<Link to="/" key={i}>
+// 			<div className="notification-item">
+// 				<i className={item.icon}></i>
+// 				<span>{item.content}</span>
+// 			</div>
+// 		</Link>
+// 	);
+// };
 
 const Topnav = () => {
 	const [filteredData, setFilteredData] = React.useState([]);
@@ -100,22 +100,22 @@ const Topnav = () => {
 			</div>
 			<div className="topnav__right">
 				<div className="topnav__right-item">
-					<Dropdown
+					{/* <Dropdown
 						// icon="bx bx-user"
 						customToggle={() => renderUserToggle(curr_user)}
 						contentData={user_menu}
 						renderItems={(item, i) => renderUserMenu(item, i)}
-					/>
+					/> */}
 					{/* {drop down here} */}
 				</div>
 				<div className="topnav__right-item">
-					<Dropdown
+					{/* <Dropdown
 						icon="bx bx-bell"
 						badge="12"
 						contentData={notifications}
 						renderItems={(item, index) => renderNotificationItem(item, index)}
 						renderFooter={() => <Link to="/">View All</Link>}
-					/>
+					/> */}
 				</div>
 				{/* <div className="topnav__right-item">
 					<ThemeMenu />
