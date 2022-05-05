bf875df3d7e2403fc1ee0c49696f5c64586ed1ca
Tri Hoang Minh <tri.hoang.hcmut@gmail.com>
2022-05-05 12:14:32 +0700

    Finish layout monthly plan tracking

--- src/pages/planTracking/trackingMonthly/trackingMonthlyInjection/TrackingMonthlyInjection.jsx
+++ src/pages/planTracking/trackingMonthly/trackingMonthlyInjection/TrackingMonthlyInjection.jsx
@@ -1,8 +1,34 @@
+import { format } from 'date-fns';
 import React from 'react';
+import { injectionApi } from '../../../../api/axios/injectionReport';
+import EmptyPlaceholder from '../../../../components/emptyPlaceholder/EmptyPlaceholder';
+import LoadingComponent from '../../../../components/loadingComponent/LoadingComponent';
 import MonthlyProgressFilter from '../../../../components/monthlyProgressFilter/MonthlyProgressFilter';
 function TrackingMonthlyInjection() {
+	const [loading, setLoading] = React.useState(false);
+	const [error, setError] = React.useState(null);
+	const [data, setData] = React.useState([]);
+	const request = React.useCallback(
+		async (startTime) => {
+			setLoading(true);
+			setError(null);
+			try {
+				const res = await injectionApi.getTemporaryInjectionPlanTracking(startTime, format(Date.now(), 'yyyy-MM-dd'));
+				setLoading(false);
+				setError(null);
+				setData(res.data);
+			} catch (err) {
+				setLoading(false);
+				setError(`Có lỗi xảy ra, vui lòng thử lại:\n${err}`);
+			}
+		},
+		[setLoading, setError]
+	);
+	React.useEffect(() => {
+		request(format(Date.now(), 'yyyy-MM').concat('-01'));
+	}, [request]);
 	const onSubmit = (e) => {
-		console.log(e);
+		request(e);
 	};
 	return (
 		<>
@@ -18,6 +44,20 @@ function TrackingMonthlyInjection() {
 					</div>
 				</div>
 			</div>
+			{loading ? (
+				<LoadingComponent className="" />
+			) : error ? (
+				<EmptyPlaceholder isError={true} msg={error} />
+			) : data ? (
+				<div className="card">
+					<div className="card__header">
+						<h3>Kết quả tìm kiếm</h3>
+					</div>
+					<div className="card__body">
+						<code>{JSON.stringify(data, null, 2)}</code>
+					</div>
+				</div>
+			) : null}
 		</>
 	);
 }
--- src/pages/planTracking/trackingMonthly/trackingMonthlyPacking/TrackingMonthlyPacking.jsx
+++ src/pages/planTracking/trackingMonthly/trackingMonthlyPacking/TrackingMonthlyPacking.jsx
@@ -1,11 +1,35 @@
+import { format } from 'date-fns';
 import React from 'react';
+import { packingApi } from '../../../../api/axios/packingReport';
+import EmptyPlaceholder from '../../../../components/emptyPlaceholder/EmptyPlaceholder';
+import LoadingComponent from '../../../../components/loadingComponent/LoadingComponent';
 import MonthlyProgressFilter from '../../../../components/monthlyProgressFilter/MonthlyProgressFilter';
 
 function TrackingMonthlyPacking() {
 	const [loading, setLoading] = React.useState(false);
 	const [error, setError] = React.useState(null);
+	const [data, setData] = React.useState([]);
+	const request = React.useCallback(
+		async (startTime) => {
+			setLoading(true);
+			setError(null);
+			try {
+				const res = await packingApi.getTemporaryPackingPlanTracking(startTime, format(Date.now(), 'yyyy-MM-dd'));
+				setLoading(false);
+				setError(null);
+				setData(res.data);
+			} catch (err) {
+				setLoading(false);
+				setError(`Có lỗi xảy ra, vui lòng thử lại:\n${err}`);
+			}
+		},
+		[setLoading, setError]
+	);
+	React.useEffect(() => {
+		request(format(Date.now(), 'yyyy-MM').concat('-01'));
+	}, [request]);
 	const onSubmit = (e) => {
-		console.log(e);
+		request(e);
 	};
 	return (
 		<>
@@ -21,6 +45,20 @@ function TrackingMonthlyPacking() {
 					</div>
 				</div>
 			</div>
+			{loading ? (
+				<LoadingComponent className="" />
+			) : error ? (
+				<EmptyPlaceholder isError={true} msg={error} />
+			) : data ? (
+				<div className="card">
+					<div className="card__header">
+						<h3>Kết quả tìm kiếm</h3>
+					</div>
+					<div className="card__body">
+						<code>{JSON.stringify(data, null, 2)}</code>
+					</div>
+				</div>
+			) : null}
 		</>
 	);
 }
